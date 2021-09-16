f9060823f17cab3481c543f513e3c5649dc64298
LJPayne98 <90472950+LJPayne98@users.noreply.github.com>
2021-09-16 10:58:44 +0100

    Update camera controller to fix console error
    creating new object of camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {   
     public Transform cameraTransform;
-    public Camera camera;
+    public new Camera camera;
 
     public float normalSpeed;
     public float fastSpeed;
